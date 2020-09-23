9b4f814787576493a817a230de8e29569e94b2a7
iNekox3 <inekox3@hotmail.com>
2020-09-23 17:21:11 +0800

    Updates

---
+++ precache-manifest.e2fa4241a8790fbe9b38ee0efed6e230.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "425b0d59841165fefad9db9062615d41",
+    "url": "/ui-todo-tutorial/index.html"
+  },
+  {
+    "revision": "4d47e0f02d9b2c9b852a",
+    "url": "/ui-todo-tutorial/static/css/main.d2ba111d.chunk.css"
+  },
+  {
+    "revision": "e80fe3c6221650a7be1c",
+    "url": "/ui-todo-tutorial/static/js/2.29376df3.chunk.js"
+  },
+  {
+    "revision": "7bfaa32b315fe466d84ab2ae49052d17",
+    "url": "/ui-todo-tutorial/static/js/2.29376df3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d47e0f02d9b2c9b852a",
+    "url": "/ui-todo-tutorial/static/js/main.5a28c57d.chunk.js"
+  },
+  {
+    "revision": "ff9ff1abb2c1f2c1bfcf",
+    "url": "/ui-todo-tutorial/static/js/runtime-main.86cf155b.js"
+  }
+]);
